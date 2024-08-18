ea43364cef577dae1f39158cb31a4fe00761cdf2
UdayaSri0 <udayasriknt2001@gmail.com>
2024-08-18 11:43:47 +0530

    Initial commit for HNDCOM15 project

---
+++ W1/Mail.py
@@ -0,0 +1,24 @@
+X = 10 #int
+Y = 215 #int
+
+Z = X + Y
+W = Z / 2
+
+print("Total is", Z)
+print("Average is", W)
+
+
+#Program 1 with input 
+
+name = input("Enter your name: ")
+
+print(f"{name}, welcome to the world of programming! once you start debugging, you'll never find your 'pees' again. 😄")
+
+#Program 2 with input int 
+N1 = int(input("Enter first number: "))
+N2 = int(input("Enter second number: "))
+
+print(N1 + N2)
+
+print(f"Sum of {N1} and {N2} is {N1 + N2}")
+
